272a84bf40239df37891b1e971a00855419f9224
RickWinters <rickwinters12@hotmail.com>
2019-04-23 15:22:04 +0200

    master branch

--- src/test.java
+++ src/test.java
@@ -5,10 +5,16 @@ public class test {
         System.out.println("test 3 mariska");
         System.out.println("Dit is Fernon");
         System.out.println("hallo");
+        System.out.println("hoi");
 
 
         System.out.println("Begin 2 alinea");
         System.out.println("ik wilde eigenlijk 2 + 2 doen");
         System.out.println("Dit hoop ik dat het werkt");
+
+
+        /*
+        hier zijn mijn regels etc
+         */
     }
 }
