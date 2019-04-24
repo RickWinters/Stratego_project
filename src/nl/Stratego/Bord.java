ba52ebea7344f6fe443eb4238cc066c801388eac
HorstMF <m.f.terhorst@gmail.com>
2019-04-24 10:05:05 +0200

    getters+setters bord

--- src/nl/Stratego/Bord.java
+++ src/nl/Stratego/Bord.java
@@ -1,4 +1,45 @@
 package nl.Stratego;
 
 public class Bord {
+
+    private long id;
+    private String naam;
+
+    int[][] SpeelStukken = new int [10][10];
+
+    //methodes
+    //check inbouwen voor move functie vlag en bom + checkbound
+    public void move(){
+        System.out.println("het spelstuk beweegt naar");
+    }
+
+    public void print(){
+        System.out.println("print het bord");
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public int[][] getSpeelStukken() {
+        return SpeelStukken;
+    }
+
+    public void setSpeelStukken(int[][] speelStukken) {
+        SpeelStukken = speelStukken;
+    }
 }
+
+
