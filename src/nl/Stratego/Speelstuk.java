9754a76439311869884d5c94d602dc6b5dbde5a8
RickWinters <rickwinters12@hotmail.com>
2019-04-24 09:37:49 +0200

    constructor in speelstuk met een lege method attack.

--- src/nl/Stratego/Speelstuk.java
+++ src/nl/Stratego/Speelstuk.java
@@ -1,4 +1,14 @@
 package nl.Stratego;
 
 public class Speelstuk {
+    public int value;
+    public String naam;
+
+    public Speelstuk(int value, String naam){
+        this.value = value;
+        this.naam = naam;
+    }
+    public void Attack(){
+        System.out.println("speelstuk valt aan");
+    }
 }
