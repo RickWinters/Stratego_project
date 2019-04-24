2cee8da7c61d190e7d3b1a851a6b2b79e9079215
HorstMF <m.f.terhorst@gmail.com>
2019-04-24 16:22:26 +0200

    applicatie aangemaakt

---
+++ src/nl/Stratego/Applicatie.java
@@ -0,0 +1,13 @@
+package nl.Stratego;
+
+public class Applicatie {
+
+    public static void main(String[] args) {
+        Speler 1 = new Speler();
+        Speler 2 - new Speler();
+
+        Bord 1 = new Bord();
+        
+
+    }
+}
